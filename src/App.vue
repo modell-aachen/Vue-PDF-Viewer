59ae0a501ddd3e936aca26a9aef9b1d8bea97da1
ArturLess <arturless109@gmail.com>
2022-11-30 16:31:10 +0100

    Adds PDFViewer Comp (don't work yet)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <PDFViewerVue :url="url" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PDFViewerVue from './components/PDFViewer.vue';
 
 export default {
-  name: 'App',
+  name: 'PDFViewer',
   components: {
-    HelloWorld
-  }
+    PDFViewerVue
+  },
+  data() {
+    return {
+      url:"http://127.0.0.1:8080/sample.pdf"
+    }
+  },
 }
 </script>
 
