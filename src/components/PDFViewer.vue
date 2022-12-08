8a84f38c4149575e793931695bbf64232a88eb0f
Ven109 <arturless109@gmail.com>
2022-12-08 14:52:26 +0100

    Viewer (#2)

--- src/components/PDFViewer.vue
+++ src/components/PDFViewer.vue
@@ -1,10 +1,14 @@
 <template>
-  <canvas ref="pdf" class="c-qwiki-pdf" />
+  <div ref="viewerContainer" id="viewerContainer" class="viewerContainer">
+    <div ref="viewer" id="viewer" class="pdfViewer"></div>
+  </div>
 </template>
 
 <script>
 import * as pdf from "pdfjs-dist/legacy/build/pdf.js";
 import PdfWorker from "pdfjs-dist/legacy/build/pdf.worker.js";
+import * as pdfjsViewer from "pdfjs-dist/legacy/web/pdf_viewer.js";
+import SANDBOX_BUNDLE_SRC from "pdfjs-dist/legacy/build/pdf.sandbox.js";
 
 pdf.GlobalWorkerOptions.workerPort = new PdfWorker();
 
@@ -18,40 +22,69 @@ export default {
   data() {
     return {
       document: null,
+      pdfViewer: null,
+      eventBus: null,
     };
   },
-  async created() {
+  async mounted() {
     await this.loadDocument();
-    if (this.document) {
-      await this.renderPage(1);
-    }
+    this.renderPDF();
   },
   methods: {
     async loadDocument() {
       try {
-        this.document = await pdf.getDocument(this.url).promise;
+        this.document = await pdf.getDocument({
+          url: this.url,
+          enableXfa: true,
+        }).promise;
       } catch (e) {
         console.warn("Failed to load document");
       }
     },
-    async renderPage(number) {
-      try {
-        const page = await this.document.getPage(number);
-        const canvas = this.$refs.pdf;
-        const viewport = page.getViewport({ scale: 1 });
-        canvas.width = viewport.width;
-        canvas.height = viewport.height;
-        await page.render({
-          canvasContext: canvas.getContext("2d"),
-          viewport,
-        }).promise;
-      } catch (e) {
-        console.warn(`Rendering of page failed`, e);
-      }
+    renderPDF() {
+      const container = this.$refs.viewerContainer;
+      const eventBus = new pdfjsViewer.EventBus();
+      const pdfLinkService = new pdfjsViewer.PDFLinkService({
+        eventBus,
+      });
+
+      // (Optionally) enable find controller.
+      const pdfFindController = new pdfjsViewer.PDFFindController({
+        eventBus,
+        linkService: pdfLinkService,
+      });
+
+      // (Optionally) enable scripting support.
+      const pdfScriptingManager = new pdfjsViewer.PDFScriptingManager({
+        eventBus,
+        sandboxBundleSrc: SANDBOX_BUNDLE_SRC,
+      });
+      this.pdfViewer = new pdfjsViewer.PDFViewer({
+        container,
+        eventBus,
+        linkService: pdfLinkService,
+        findController: pdfFindController,
+        scriptingManager: pdfScriptingManager,
+      });
+      pdfLinkService.setViewer(this.pdfViewer);
+      pdfScriptingManager.setViewer(this.pdfViewer);
+      eventBus.on("pagesinit", () => {
+        this.pdfViewer.currentScaleValue = "page-width";
+      });
+      this.pdfViewer.setDocument(this.document);
+      pdfLinkService.setDocument(this.document, null);
+      this.eventBus = eventBus;
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
+@import "../../node_modules/pdfjs-dist/legacy/web/pdf_viewer.css";
+.viewerContainer {
+  overflow: auto;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
 </style>
